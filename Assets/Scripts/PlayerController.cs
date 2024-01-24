643669330cceca695e9440fd633f0e99f557e18b
33634 <lefevre.jeanne.emma@gmail.com>
2024-01-24 11:23:05 +0100

    Add control to the car

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Moving our car foraward
         // Using transform here means that we are using our transform component of our object (whihch the script is attached to)
         // Both lines are doing the same things
         // 20 meters every seconds going forward (if we don't multiply by a number it means it's moving a meters by seconds)
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // transform.Translate(0, 0, 0.25f);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
